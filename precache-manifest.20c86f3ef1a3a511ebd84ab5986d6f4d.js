1640f8707444e9925f837959cd4a04f41a5af180
hoalongnatsu <hoalongnatsu@gmail.com>
2019-08-07 23:21:22 +0700

    first commit

---
+++ precache-manifest.20c86f3ef1a3a511ebd84ab5986d6f4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0fb82473b444dcf0daed",
+    "url": "/static/js/main.0fb82473.chunk.js"
+  },
+  {
+    "revision": "3afb3759ed8701d83f3e",
+    "url": "/static/js/2.3afb3759.chunk.js"
+  },
+  {
+    "revision": "4b37e7525b4e0abaf11a7bdce203e7e7",
+    "url": "/index.html"
+  }
+];
